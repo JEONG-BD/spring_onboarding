46aa0a32a5ee39aaec339f93bcb964e40d18fc98
JEONG-BD <dev.byeongdu@gmail.com>
2025-12-31 23:57:22 +0900

    인메모리 todo controller 구현

---
+++ todo-in-memory/src/main/java/com/asfg/todo/controller/TodoController.java
@@ -0,0 +1,104 @@
+package com.asfg.todo.controller;
+
+import com.asfg.todo.model.Todo;
+import com.asfg.todo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos/v1")
+public class TodoController {
+
+    @Autowired
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public Todo save(Todo todo){
+        return todoService.save(todo);
+    }
+
+    @GetMapping
+    @Operation(summary = "전체 작업 조회", description = "전체 잔업 조히")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "204", description = "작업 없음")
+    })
+    public ResponseEntity<List<Todo>> getAllTodo(){
+        List<Todo> todos = todoService.findAll();
+        if(todos.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(todos);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "작업 조회", description = "ID로 작업 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "작업 없음")
+    })
+    public ResponseEntity<Todo> findById(@PathVariable Long id){
+        Todo todo = todoService.findById(id);
+        if(todo == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(todo);
+    }
+
+
+
+    @PostMapping
+    @Operation(summary = "작업 생성", description = "새 작업 생성")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "생성 완료")
+    })
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo){
+        Todo result = todoService.save(todo);
+        return ResponseEntity.status(201).body(result);
+    }
+
+
+    @PutMapping("/{id}")
+    @Operation(summary = "작업 수정", description = "ID로 작업 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "작업 없음"),
+    })
+    public ResponseEntity<Todo> updateTodo(@PathVariable Long id,
+                                           @RequestBody Todo todo){
+        Todo existingTodo = todoService.findById(id);
+        if(existingTodo == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(todoService.update(id, todo));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "작업 삭제", description = "ID로 작업 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "내용 없음"),
+            @ApiResponse(responseCode = "404", description = "작업 없음")
+    })
+
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id){
+        Todo existingTodo = todoService.findById(id);
+        if(existingTodo == null){
+            return ResponseEntity.notFound().build();
+        }
+        todoService.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+
+}

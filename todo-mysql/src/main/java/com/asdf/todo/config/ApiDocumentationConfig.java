2acef26fcb766dd758b1436f537f8a4795ea02c3
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-03 14:52:55 +0900

    config 수정

---
+++ todo-mysql/src/main/java/com/asdf/todo/config/ApiDocumentationConfig.java
@@ -0,0 +1,19 @@
+package com.asdf.todo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiDocumentationConfig {
+
+    @Bean
+    public OpenAPI apiDocumentation(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Todo List API")
+                        .version("2.0")
+                        .description("Spring Boot3를 이용한 ToDo List API"));
+    }
+}

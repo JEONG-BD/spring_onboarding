11a6382634952e4ca03e4c51369580d7476354ad
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-06 13:51:26 +0900

    repository 구현

---
+++ minilog-mysql/src/main/java/com/adsf/minilog/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.adsf.minilog.repository;
+
+import com.adsf.minilog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}

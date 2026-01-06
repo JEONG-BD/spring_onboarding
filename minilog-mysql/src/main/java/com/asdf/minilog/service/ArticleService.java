434603b134038750d594ab1fadf2a7bd99a34380
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-06 15:50:08 +0900

    service 구현

---
+++ minilog-mysql/src/main/java/com/asdf/minilog/service/ArticleService.java
@@ -0,0 +1,21 @@
+package com.asdf.minilog.service;
+
+import com.asdf.minilog.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(isolation = Isolation.REPEATABLE_READ)
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+
+}
---
+++ minilog-mysql/src/main/java/com/asdf/minilog/service/UserService.java
@@ -0,0 +1,71 @@
+package com.asdf.minilog.service;
+
+import com.asdf.minilog.dto.UserRequestDto;
+import com.asdf.minilog.dto.UserResponseDto;
+import com.asdf.minilog.entity.User;
+import com.asdf.minilog.exception.UserNotFoundException;
+import com.asdf.minilog.repository.UserRepository;
+import com.asdf.minilog.util.EntityDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponseDto> getUsers(){
+        return userRepository.findAll().stream()
+                .map(EntityDtoMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<UserResponseDto> getUserById(Long id){
+        return userRepository.findById(id).map(EntityDtoMapper::toDto);
+    }
+
+    public UserResponseDto createUser(UserRequestDto dto){
+        if(userRepository.findByUsername(dto.getUsername()).isPresent()){
+            throw new UserNotFoundException("이미 존재하는 사용자 이름");
+        }
+
+        User savedUser = userRepository.save(User.builder()
+                .username(dto.getUsername())
+                .password(dto.getUsername()).build());
+
+        return EntityDtoMapper.toDto(savedUser);
+    }
+
+    public UserResponseDto updateUser(Long userId, UserRequestDto dto){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(
+                        String.format("해당 아이디 (%d)를 가진 사용자를 찾을 없습니다", userId)));
+
+        user.setUsername(dto.getUsername());
+        user.setPassword(dto.getPassword());
+        var updatedUser = userRepository.save(user);
+        return EntityDtoMapper.toDto(updatedUser);
+    }
+
+    public void deleteUser(Long userId){
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(
+                        String.format("해당 아이디 (%d)를 가진 사용자를 찾을 수 없습니다", userId)
+                ));
+
+        userRepository.deleteById(existingUser.getId());
+    }
+}

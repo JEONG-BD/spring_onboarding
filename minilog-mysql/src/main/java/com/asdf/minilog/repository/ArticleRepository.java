8f0f78ba1465eb638c95704c336dbbccbc66d05c
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-07 17:25:26 +0900

    ArticleRepository 수정

--- minilog-mysql/src/main/java/com/asdf/minilog/repository/ArticleRepository.java
+++ minilog-mysql/src/main/java/com/asdf/minilog/repository/ArticleRepository.java
@@ -1,6 +1,6 @@
 package com.asdf.minilog.repository;
 
-import com.adsf.minilog.entity.Article;
+import com.asdf.minilog.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;

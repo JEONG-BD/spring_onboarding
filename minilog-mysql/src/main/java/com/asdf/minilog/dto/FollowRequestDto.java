c90e027f90afbdbdd484fc409d43de0750cd11ce
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-12 19:45:48 +0900

    dto 수정

--- minilog-mysql/src/main/java/com/asdf/minilog/dto/FollowRequestDto.java
+++ minilog-mysql/src/main/java/com/asdf/minilog/dto/FollowRequestDto.java
@@ -1,5 +1,6 @@
 package com.asdf.minilog.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
@@ -7,7 +8,11 @@ import lombok.NonNull;
 @Builder
 @Data
 public class FollowRequestDto {
-
+  @Deprecated(since = "2.0", forRemoval = true)
+  @Schema(description = "작성자 ID(이 필드는 더이상 사용되지 않습니다)",
+          example = "0",
+          required = true,
+          deprecated = true)
   @NonNull private Long followerId;
   @NonNull private Long followeeId;
 }
